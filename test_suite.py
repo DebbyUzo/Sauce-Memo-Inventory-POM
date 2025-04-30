3ac68cbbd8061d720c723a8c0f37aa566489361b
unknown <you@example.com>
2025-04-30 11:27:56 +0100

    update

---
+++ test_suite.py
@@ -0,0 +1,36 @@
+import pytest
+from selenium import webdriver
+
+from ActionPage.Action import Action_Page
+
+@pytest.fixture(scope="session")
+def driver_setup():
+    driver = webdriver.Chrome()
+    driver.implicitly_wait(20)
+    driver.maximize_window()
+    yield driver
+    driver.quit()
+
+# @`pytest.fixture(scope="session")
+# #def login(driver_setup):
+#     #driver = driver_setup
+#     login_page = Action_Page(driver)
+#     login_page.open_login_page("https://www.saucedemo.com/")
+#     return login_page
+
+@pytest.fixture(scope="session")
+def login(driver_setup):
+    driver = driver_setup
+    login_page = Action_Page(driver)
+    login_page.login_url("https://www.saucedemo.com/")
+    return login_page
+
+def test_login_page_on_automation_customer_service_website(login):
+    login.enter_username("standard_user")
+    login.enter_password("secret_sauce")
+    login.click_submit_button()
+
+def test_swag_labs_page_on_automation_swag_labs_website(Produects):
+    login.enter_username("standard_user")
+    login.enter_password("secret_sauce")
+    login.click_submit_button()
